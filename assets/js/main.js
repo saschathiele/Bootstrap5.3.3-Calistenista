023c7125d83cf3117411ed934060118672f17a36
Sascha Thiele <saschathiele@hotmail.de>
2024-11-11 12:54:47 -0300

    added a backToTopBtn function
    added a backToTopBtn function

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,21 @@ uniteToastBtn.addEventListener('click', (event) => {
     jsConfetti.addConfetti();
 });
 
+
+
+let myBackToTopBtn = document.getElementById('backToTopBtn');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myBackToTopBtn.style.display = "block";
+  } else {
+    myBackToTopBtn.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
